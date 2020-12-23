be9b3fc124a28006ce65a301de0273c74f86002e
M-Nardi <marcellonardi09@gmail.com>
2020-12-23 18:49:33 -0300

    acrescentado a mensagem de como voce esta hoje

--- AulaGit/Program.cs
+++ AulaGit/Program.cs
@@ -8,6 +8,7 @@ namespace AulaGit
         {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("bom dia!");
+            Console.WriteLine("Como voce está hoje?");
 
             Console.WriteLine();
         }
