d0abc24a18f11806fc12f579ec9c9e075f73005c
M-Nardi <marcellonardi09@gmail.com>
2020-12-23 18:30:06 -0300

    Criado a primeira versao Ola Mundo

---
+++ AulaGit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AulaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+
+            Console.WriteLine();
+        }
+    }
+}
